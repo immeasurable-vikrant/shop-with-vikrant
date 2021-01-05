4d210c8df1ef1c218f8111f47f0500e9b93c8d91
Vikrant Singh Bandral <vikrant.immeasurable@gmail.com>
2021-01-06 01:44:25 +0530

    Updated ShopData and also cart utils to group items in cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.action';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
   return (
     <div className='collection-item'>
       <div className='image' style={{ backgroundImage: `url(${imageUrl}` }} />
@@ -10,8 +15,13 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>ADD TO CART</CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,9 @@
-import { CartActionTypes } from './cart.types'
+import { CartActionTypes } from './cart.types';
+import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -9,11 +11,17 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
-        hidden : !state.hidden
+        hidden: !state.hidden,
       };
-      default:
-      return state
+
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload )
+      };
+    default:
+      return state;
   }
 };
 
-export default cartReducer
+export default cartReducer;
