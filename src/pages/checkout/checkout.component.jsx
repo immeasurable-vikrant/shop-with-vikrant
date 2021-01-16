5be0bab0e4a2c27759e5863a254215ea9296e6ae
Vikrant Singh Bandral <vikrant.immeasurable@gmail.com>
2021-01-17 00:28:53 +0530

    Added AddItem and RemoveItem functionality in Checkout Page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -27,7 +27,7 @@ const CheckoutPage = ({ cartItems, total}) => {
         </div>
       </div>
       {
-        cartItems.map(cartItem => <CheckoutItem cartItem={cartItem} /> )
+        cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} /> )
       }
       <div className='total'>
         <span>Total: ₹{total}</span>
