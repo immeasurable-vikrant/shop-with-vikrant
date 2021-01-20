fe4b54fcec28096eb6ad1451450c6363f6b14d4d
Vikrant Singh Bandral <vikrant.immeasurable@gmail.com>
2021-01-20 20:10:42 +0530

    Added Redux Persist in store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-/** @format */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+
+import {store, persistor } from './redux/store';
+
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
